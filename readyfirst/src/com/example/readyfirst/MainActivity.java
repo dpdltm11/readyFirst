33f33f249583f23420e90c0e64c253c43747db0d
dpdltm11 <Leesangwon0114@gmail.com>
2015-01-03 09:46:45 +0900

    대략layout

--- readyfirst/src/com/example/readyfirst/MainActivity.java
+++ readyfirst/src/com/example/readyfirst/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.readyfirst;
 
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-
+	
+	private FragmentTabHost mTabHost;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("주행모드").setIndicator("주행모드", null),
+                driveMode.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("분석모드").setIndicator("분석모드", null),
+                analysisMode.class, null);
+		
+        for (int i = 0; i < mTabHost.getTabWidget().getChildCount(); i++) {
+
+	        final TextView tv = (TextView) mTabHost.getTabWidget().getChildAt(i)
+	                .findViewById(android.R.id.title);
+
+	            // Look for the title view to ensure this is an indicator and not a divider.(I didn't know, it would return divider too, so I was getting an NPE)
+	        if (tv == null)
+	            continue;
+	        else
+	        {
+	        	tv.setTextSize(15);
+	            tv.setTextColor(Color.WHITE);
+	        }
+		}
 	}
 
 	@Override
